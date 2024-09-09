4d781e92575c36d84bf7d1cf20aba4458ab22e10
nmohanu <nmohanu@gmail.com>
2024-09-09 17:30:37 +0200

    problem-3

--- problem-3.cpp
+++ problem-3.cpp
@@ -1,3 +1,34 @@
-file :              /// home/nmohanu/Documents/dev/euler/problem-1.cpp
-       file :       /// home/nmohanu/Documents/dev/euler/problem-2.cpp
-              file: /// home/nmohanu/Documents/dev/euler/problem-3.cpp
+#include <iostream>
+
+bool isPrime(int n) {
+  if (n <= 1)
+    return false;
+  for (int i = 2; i <= n / 2; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+int main() {
+  long number = 600851475143;
+  long remainder = number;
+  int smallestPrime = 2;
+
+  for (int i = 2; i < number; i++) {
+    if (isPrime(i)) {
+      if (remainder % i == 0) {
+        remainder /= i;
+        smallestPrime = i;
+      }
+    }
+
+    if (remainder == 1) {
+      break;
+    }
+  }
+
+  std::cout << smallestPrime;
+  return 0;
+}
